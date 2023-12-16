3615e65a41a46d0b03d973fee99c54e640a111f6
Michael Vytlingam <michael.vytlingam@gmail.com>
2023-12-16 00:17:22 -0800

    add playing card and stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from "react";
-import {
-  Button,
-  Card,
-  Col,
-  Form,
-  Input,
-  Menu,
-  MenuProps,
-  message,
-  Row,
-  Space,
-  Typography,
-  Upload,
-  UploadFile,
-} from "antd";
-import { CopyOutlined, UploadOutlined } from "@ant-design/icons";
+import { Button, Card, Form, Input, Typography } from "antd";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { startPeer, stopPeerSession } from "./store/peer/peerActions";
 import * as connectionAction from "./store/connection/connectionActions";
-import { DataType, PeerConnection } from "./helpers/peer";
-import { useAsyncState } from "./helpers/hooks";
-import Paragraph from "antd/es/skeleton/Paragraph";
+import { PeerConnection } from "./helpers/peer";
 
 const { Title } = Typography;
 
@@ -55,7 +38,7 @@ export const App: React.FC = () => {
 
   return (
     <div className="App">
-      {!peer.started ? (
+      {/* {!peer.started ? (
         <Card title="Dutch Blitz">
           <Form name="basic" onFinish={handleStartSession}>
             <Form.Item<FieldType>
@@ -91,14 +74,61 @@ export const App: React.FC = () => {
               </Button>
             </Form.Item>
           </Form>
-          <Button>Create Game</Button>
           <Title level={4}>ID: {peer.id}</Title>
           <Title level={4}>Name: {peer.name}</Title>
           {connection.list.length > 0 && connection.list}
           <Button onClick={handleStopSession}>End Game</Button>
         </Card>
-      )}
+      )} */}
+      <PlayingCardStack cards={[{ digit: 4, color: "red" }]} />
     </div>
+  );
+};
+
+interface PlayingCard {
+  digit: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+  color: "red" | "yellow" | "green" | "blue";
+}
+
+export const PlayingCard = ({
+  card,
+  style,
+}: {
+  card: PlayingCard;
+  style: React.CSSProperties;
+}) => {
+  return (
+    <Card
+      bordered={false}
+      style={{
+        backgroundColor: card.color,
+        color: "white",
+        fontSize: "6rem",
+        border: 0,
+        width: "5rem",
+        height: "7rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        userSelect: "none",
+        ...style,
+      }}
+    >
+      {card.digit}
+    </Card>
+  );
+};
+
+export const PlayingCardStack = ({ cards }: { cards: PlayingCard[] }) => {
+  return (
+    <PlayingCard
+      style={{
+        boxShadow:
+          "-1px 1px 0px 0px #FFF, -3px 3px 0px 0px #000000 -5px 5px 0px 0px #FFF, -7px 7px 0px 0px #000000",
+        borderRadius: "8px",
+      }}
+      card={cards[0]}
+    />
   );
 };
 
