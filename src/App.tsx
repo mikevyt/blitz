3d4977a3a7965622de6c82b43d962e0d6eecda28
Michael Vytlingam <michael.vytlingam@gmail.com>
2023-12-15 23:39:57 -0800

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -23,23 +23,6 @@ import { useAsyncState } from "./helpers/hooks";
 import Paragraph from "antd/es/skeleton/Paragraph";
 
 const { Title } = Typography;
-type MenuItem = Required<MenuProps>["items"][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: "group"
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  } as MenuItem;
-}
 
 type FieldType = {
   name?: string;
@@ -68,44 +51,6 @@ export const App: React.FC = () => {
   const handleStopSession = async () => {
     await PeerConnection.closePeerSession();
     dispatch(stopPeerSession());
-  };
-
-  const handleConnectOtherPeer = () => {
-    connection.id != null
-      ? dispatch(connectionAction.connectPeer(connection.id || ""))
-      : message.warning("Please enter ID");
-  };
-
-  const [fileList, setFileList] = useAsyncState([] as UploadFile[]);
-  const [sendLoading, setSendLoading] = useAsyncState(false);
-
-  const handleUpload = async () => {
-    if (fileList.length === 0) {
-      message.warning("Please select file");
-      return;
-    }
-    if (!connection.selectedId) {
-      message.warning("Please select a connection");
-      return;
-    }
-    try {
-      await setSendLoading(true);
-      let file = fileList[0] as unknown as File;
-      let blob = new Blob([file], { type: file.type });
-
-      await PeerConnection.sendConnection(connection.selectedId, {
-        dataType: DataType.FILE,
-        file: blob,
-        fileName: file.name,
-        fileType: file.type,
-      });
-      await setSendLoading(false);
-      message.info("Send file successfully");
-    } catch (err) {
-      await setSendLoading(false);
-      console.log(err);
-      message.error("Error when sending file");
-    }
   };
 
   return (
@@ -149,6 +94,8 @@ export const App: React.FC = () => {
           <Button>Create Game</Button>
           <Title level={4}>ID: {peer.id}</Title>
           <Title level={4}>Name: {peer.name}</Title>
+          {connection.list.length > 0 && connection.list}
+          <Button onClick={handleStopSession}>End Game</Button>
         </Card>
       )}
     </div>
