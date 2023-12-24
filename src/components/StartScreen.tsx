29d43a645ab3c20c0d5128093f7ef861faf2bed0
Michael Vytlingam <michael.vytlingam@gmail.com>
2023-12-24 16:24:01 -0500

    delete old code

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -21,21 +21,6 @@ export const StartScreen = () => {
   const connection = useAppSelector((state) => state.connection);
   const dispatch = useAppDispatch();
 
-  //   const handleStartSession = (values: FieldType) => {
-  //     console.log({ values });
-  //     if (!values?.name) {
-  //       return;
-  //     }
-  //     dispatch(startPeer(values.name));
-  //   };
-
-  //   const handleJoinGame = (values: { code?: string }) => {
-  //     if (!values.code) {
-  //       return;
-  //     }
-  //     dispatch(connectPeer(values.code));
-  //   };
-
   const handleStopSession = async () => {
     await PeerConnection.closePeerSession();
     dispatch(stopPeerSession());
@@ -80,46 +65,3 @@ export const StartScreen = () => {
     </Card>
   );
 };
-
-//   return !peer.started ? (
-//     <Card title="Dutch Blitz">
-//       <Form name="basic" onFinish={handleStartSession}>
-//         <Form.Item<FieldType>
-//           label="Name"
-//           name="name"
-//           rules={[{ required: true, message: "Please input your name!" }]}
-//         >
-//           <Input />
-//         </Form.Item>
-
-//         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-//           <Button type="primary" htmlType="submit">
-//             Start
-//           </Button>
-//         </Form.Item>
-//       </Form>
-//     </Card>
-//   ) : (
-//     <Card>
-//       <Form name="basic" onFinish={handleJoinGame}>
-//         <Form.Item<FieldType>
-//           label="Code"
-//           name="code"
-//           rules={[{ required: true, message: "Please input your code!" }]}
-//         >
-//           <Input />
-//         </Form.Item>
-
-//         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-//           <Button>Create Game</Button>
-//           <Button type="primary" htmlType="submit">
-//             Join Game
-//           </Button>
-//         </Form.Item>
-//       </Form>
-//       <Title level={4}>ID: {peer.id}</Title>
-//       <Title level={4}>Name: {peer.name}</Title>
-//       {connection.list.length > 0 && connection.list}
-//       <Button onClick={handleStopSession}>End Game</Button>
-//     </Card>
-//   );
