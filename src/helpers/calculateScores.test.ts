74ddb06d386efcfedb847aa6b91d487c640f3488
Michael Vytlingam <michael.vytlingam@gmail.com>
2024-06-23 22:06:15 -0700

    add tests for calculate scores

--- src/helpers/calculateScores.test.ts
+++ src/helpers/calculateScores.test.ts
@@ -2,7 +2,7 @@ import { GameState } from "../store/game/gameTypes";
 import { calculateScores } from "./calculateScores";
 
 describe("calculateScores", () => {
-  it.skip("should", () => {
+  it("should calculate score for the center cards correctly", () => {
     const game: GameState = {
       center: [
         [
@@ -36,6 +36,58 @@ describe("calculateScores", () => {
       stash: { player1: [], player2: [] },
       stashVisible: { id: 1 },
     };
-    expect(calculateScores(game)).toBe(1);
+    const result = calculateScores(game);
+    expect(result.get("player1")).toEqual(2);
+    expect(result.get("player2")).toEqual(1);
+  });
+
+  it("should calculate score for the stash cards correctly", () => {
+    const game: GameState = {
+      center: [
+        [
+          {
+            color: "red",
+            digit: 1,
+            location: "center",
+            owner: "player1",
+            positive: false,
+          },
+          {
+            color: "red",
+            digit: 2,
+            location: "center",
+            owner: "player2",
+            positive: false,
+          },
+        ],
+        [
+          {
+            color: "green",
+            digit: 1,
+            location: "center",
+            owner: "player1",
+            positive: false,
+          },
+        ],
+      ],
+      stack: {
+        player1: [],
+        player2: [
+          {
+            color: "red",
+            digit: 3,
+            location: "stack",
+            owner: "player2",
+            positive: false,
+          },
+        ],
+      },
+      spread: { id: [] },
+      stash: { player1: [], player2: [] },
+      stashVisible: { id: 1 },
+    };
+    const result = calculateScores(game);
+    expect(result.get("player1")).toEqual(2);
+    expect(result.get("player2")).toEqual(-1);
   });
 });
--- src/helpers/calculateScores.ts
+++ src/helpers/calculateScores.ts
@@ -4,11 +4,11 @@ export const calculateScores = (game: GameState) => {
   const scoresMap = new Map<string, number>();
   game.center.forEach((deck) =>
     deck.forEach((card) =>
-      scoresMap.set(card.owner, scoresMap.get(card.owner) || 0 + 1)
+      scoresMap.set(card.owner, (scoresMap.get(card.owner) || 0) + 1)
     )
   );
-  scoresMap.forEach((_, key) =>
-    scoresMap.set(key, scoresMap.get(key) || 0 - 2)
+  Object.entries(game.stack).forEach(([key, cards]) =>
+    scoresMap.set(key, (scoresMap.get(key) || 0) - 2 * cards.length)
   );
   return scoresMap;
 };
