1424ce58d3e720aad179b2ee2ac1f0b2d15156ff
Michael Vytlingam <michael.vytlingam@gmail.com>
2024-06-25 22:21:21 -0700

    fix tests

--- src/helpers/setUpGameState.ts
+++ src/helpers/setUpGameState.ts
@@ -1,55 +1,33 @@
 import "object.groupby";
 import { GameState } from "../store/game/gameTypes";
-import { PlayingCard } from "../types/PlayingCard";
 import { generateCards } from "./generateCards";
 
 // for testing
-export const setUpGameState = (id: string): GameState => {
-  let cards = generateCards(id);
-  // shuffle(cards);
-  cards = Object.values(
-    cards.reduce(function (r, a) {
-      r[a.color] = r[a.color] || [];
-      r[a.color].push(a);
-      return r;
-    }, Object.create(null)) as PlayingCard[][]
-  ).flat();
-
-  console.log({ cards });
-
-  return {
-    center: [],
-    spread: {
-      [id]: cards
-        .slice(10, 13)
-        .map((card) => [{ ...card, location: "spread" }]),
-    },
-    stack: {
-      [id]: cards
-        .slice(0, 10)
-        .reverse()
-        .map((card) => ({ ...card, location: "stack" })),
-    },
-    stash: {
-      [id]: cards
-        .slice(14, cards.length)
-        .map((card) => ({ ...card, location: "stash" })),
-    },
-    stashVisible: { [id]: -1 },
-  };
-};
-
 // export const setUpGameState = (id: string): GameState => {
-//   const cards = generateCards(id);
-//   shuffle(cards);
+//   let cards = generateCards(id);
+//   // shuffle(cards);
+//   cards = Object.values(
+//     cards.reduce(function (r, a) {
+//       r[a.color] = r[a.color] || [];
+//       r[a.color].push(a);
+//       return r;
+//     }, Object.create(null)) as PlayingCard[][]
+//   ).flat();
+
+//   console.log({ cards });
 
 //   return {
 //     center: [],
 //     spread: {
-//       [id]: cards.slice(0, 3).map((card) => [{ ...card, location: "spread" }]),
+//       [id]: cards
+//         .slice(10, 13)
+//         .map((card) => [{ ...card, location: "spread" }]),
 //     },
 //     stack: {
-//       [id]: cards.slice(3, 13).map((card) => ({ ...card, location: "stack" })),
+//       [id]: cards
+//         .slice(0, 10)
+//         .reverse()
+//         .map((card) => ({ ...card, location: "stack" })),
 //     },
 //     stash: {
 //       [id]: cards
@@ -59,3 +37,24 @@ export const setUpGameState = (id: string): GameState => {
 //     stashVisible: { [id]: -1 },
 //   };
 // };
+
+export const setUpGameState = (id: string): GameState => {
+  const cards = generateCards(id);
+  shuffle(cards);
+
+  return {
+    center: [],
+    spread: {
+      [id]: cards.slice(0, 3).map((card) => [{ ...card, location: "spread" }]),
+    },
+    stack: {
+      [id]: cards.slice(3, 13).map((card) => ({ ...card, location: "stack" })),
+    },
+    stash: {
+      [id]: cards
+        .slice(14, cards.length)
+        .map((card) => ({ ...card, location: "stash" })),
+    },
+    stashVisible: { [id]: -1 },
+  };
+};
